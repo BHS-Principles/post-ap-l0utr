73f1be1106715cc0d2b2498b5e070b90806bda10
l0utr <2035426@apps.nsd.org>
2025-05-20 16:31:18 +0000

    I just learned about "inside of" aka Encapsulation+Abstraction

---
+++ public/js.js
@@ -0,0 +1,10 @@
+var Costco_Menu=[
+    "Chicken Bake",
+    "Double Chunk Chocolate Cookie",
+    "Cheese Pizza",
+    "Pepperoni Pizza"
+]
+
+for(var i=0;i<Costco_Menu.length;i++){
+    alert(Costco_Menu[i]);
+}
