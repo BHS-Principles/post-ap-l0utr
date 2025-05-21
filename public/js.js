95abe3f77d8c8ad0cb001aa46f479d814e1a6c62
l0utr <2035426@apps.nsd.org>
2025-05-21 16:41:26 +0000

    i made buttons and checked their events

--- public/js.js
+++ public/js.js
@@ -3,8 +3,28 @@ var Costco_Menu=[
     "Double Chunk Chocolate Cookie",
     "Cheese Pizza",
     "Pepperoni Pizza"
-]
+];
+
+/*for(var i=0;i<Costco_Menu.length;i++){
+    alert(Costco_Menu[i]);}
+*/
+
+function cycle_through(arr){
+    for(var n=0;n<arr.length;n++){
+        alert(arr[n]);
+    }
+}
+
+cycle_through(Costco_Menu);
+
+var btn= document.getElementById("myButton");
 
 for(var i=0;i<Costco_Menu.length;i++){
-    alert(Costco_Menu[i]);
+    var newBtn= btn.cloneNode(true);
+    newBtn.innerHTML=Costco_Menu[i];
+    document.body.append(newBtn);
+
+    newBtn.addEventListener("click", function(){
+        alert(Costco_Menu[i]);
+    });
 }
